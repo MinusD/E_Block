1c6a2567e9f5776c5c341fa488d0a302cae77e55
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-04-30 01:43:13 +0300

    Добавлена функция запроса текущего курса валют Добавлены селекторы для выбора конвертируемых валют

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
 # Запуск
 WINDOW_TITLE = 'Валютный помощник'
 WINDOW_SIZE = '500x500'
+WINDOW_FONT_SIZE = None
+
+# Общее
+PADX = 30  # Размер отступа по горизонтали
+PADY = 10  # Размер отступа по вертикали
+
+# Конвертор валют
+SORT_CURRENCY_LIST = False  # Сортировать ли рубль в списке
+RANDOM_START_CURRENCY = True  # Выбирать ли случайную валюта при запуске
--- main.py
+++ main.py
@@ -2,12 +2,28 @@ from tkinter import *
 import tkinter.ttk as ttk
 import urllib.request
 import xml.dom.minidom
+import random
 import config
+
+
+def get_currency_list() -> list:
+    currency_list = ['Российский рубль']
+    response = urllib.request.urlopen("http://cbr.ru/scripts/XML_daily.asp?date_req=22/04/2022")
+    dom = xml.dom.minidom.parse(response)  # Получение DOM структуры айла
+    dom.normalize()
+    node_array = dom.getElementsByTagName("ValCurs")  # Получили список объектов валют
+    for currency in node_array:  # Перебираем по валютам
+        for data in currency.childNodes:
+            currency_list.append(data.childNodes[3].childNodes[0].nodeValue)
+            tmp = []
+            for n in data.childNodes:
+                tmp.append(n.childNodes[0].nodeValue)
+    return sorted(currency_list) if config.SORT_CURRENCY_LIST else currency_list
 
 
 def get_data_from_cbr(date):
     answer = []
-    response = urllib.request.urlopen("http://cbr.ru/scripts/XML_daily.asp?date_req=22/04/2022")
+    response = urllib.request.urlopen("http://cbr.ru/scripts/XML_daily.asp")
     dom = xml.dom.minidom.parse(response)  # Получение DOM структуры айла
     dom.normalize()
     node_array = dom.getElementsByTagName("ValCurs")  # Получили список объектов валют
@@ -35,8 +51,10 @@ def clicked():
 
 
 if __name__ == '__main__':
-    # get_data_from_cbr(123)
+    get_currency_list()
     window = Tk()
+    # window.option_add("*Font", "courier 8")
+    # window.option_add("*Font", "default_font")
     window.title(config.WINDOW_TITLE)
     window.geometry(config.WINDOW_SIZE)
 
@@ -47,14 +65,31 @@ if __name__ == '__main__':
     tab_control.add(tab1, text="Калькулятор валют")
     tab_control.add(tab2, text="Динамика курса")
 
-    combo = ttk.Combobox(tab1)  # Создание комбобокса на первой вкладке, можно добавить аргументы, например ширину
-    combo["values"] = ["раз", "два", "три"]
-    combo.grid(column=0, row=0)  # Размещение в окне, указана позиция, можно указать отступы
+    # txt = Entry(tab1)  # Текстовое поле для ввода
+    # txt.grid(column=1, row=1)
+
+    # Селектор выбора валюты (ИЗ)
+    currency_combo_from = ttk.Combobox(tab1, state="readonly")
+    currency_combo_from['values'] = get_currency_list()
+    currency_combo_from.current(21 if config.SORT_CURRENCY_LIST else 0)  # В любом случае первая валюта рубль
+    currency_combo_from.grid(column=1, row=1, padx=config.PADX, pady=config.PADY)
+
+    # Селектор выбора валюты (В)
+    currency_combo_to = ttk.Combobox(tab1, state="readonly")
+    currency_combo_to['values'] = get_currency_list()
+    if config.RANDOM_START_CURRENCY:
+        currency_combo_to.current(random.randint(0, len(currency_combo_to['values']) - 1))
+    currency_combo_to.grid(column=1, row=2, padx=config.PADX, pady=config.PADY)
+
+    # combo = ttk.Combobox(tab1)  # Создание комбобокса на первой вкладке, можно добавить аргументы, например ширину
+    # combo["values"] = ["раз", "два", "три"]
+    # print(combo["values"].size())
+    # combo.grid(column=0, row=0)  # Размещение в окне, указана позиция, можно указать отступы
 
     txt = Entry(tab1)  # Текстовое поле для ввода
     btn = Button(tab1, text="Действие", command=clicked)  # Кнопка, действие реализуется в функции clicked
     lb1 = Label(tab1, text="123")  # Надпись
-    lb1.grid(column=1, row=2)
+    # lb1.grid(column=1, row=2)
     # tab_control.add(tab1, txt)
     # Все объекты должны размещаться функцией grid
     # Существуют функции для программного получения значения текстового поля и изменения надписи
