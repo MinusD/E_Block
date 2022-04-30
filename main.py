f62bebe998621361ae580301cf323db38890a435
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-05-01 01:14:49 +0300

    Main: Добавлена вторая страница приложения Добавлена функция обработки выбора периода для 2 случаев
    Config:
    Добавлен параметр рандома

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import tkinter.ttk as ttk
 import urllib.request
 import xml.dom.minidom
 import random
-import matplotlib  # Необходимо установить через PIP
+import datetime
+import re
+import matplotlib
 import matplotlib.pyplot as plt
 import config
 
 
-def get_currency_list() -> list:
-    currency_list = [config.RUBLE_SLUG]
+def get_currency_list(with_ruble: bool = True) -> list:
+    currency_list = [config.RUBLE_SLUG] if with_ruble else []
     response = urllib.request.urlopen(config.CBR_URL)
     dom = xml.dom.minidom.parse(response)  # Получение DOM структуры айла
     dom.normalize()
@@ -37,30 +39,26 @@ def get_current_exchange_rate(currency: str) -> float:
 def convert_currency_input_to_btn() -> None:
     try:
         value = float(currency_input.get().replace(',', '.'))
-        convert_error_label.configure(fg='black')
-        currency_from, currency_to = currency_combo_from.get(), currency_combo_to.get()
+        convert_out_label.configure(fg='black')
+        currency_from = currency_combo_from.get()
+        currency_to = currency_combo_to.get()
         if currency_from and currency_to:
-            convert_error_label.configure(
+            convert_out_label.configure(
                 text=(1 / get_current_exchange_rate(currency_from)) * get_current_exchange_rate(currency_to) * value)
             # print(currency_from, '->', currency_to)
             # print((1 / get_current_exchange_rate(currency_from)) * get_current_exchange_rate(currency_to))
             # print('======')
         else:
-            convert_error_label.configure(fg='red')
-            convert_error_label.configure(text=config.NO_END_CURRENCY_SELECTED_ERROR)
+            convert_out_label.configure(fg='red')
+            convert_out_label.configure(text=config.NO_END_CURRENCY_SELECTED_ERROR)
     except ValueError:
-        convert_error_label.configure(fg='red')
-        convert_error_label.configure(text=config.NOT_NUMERIC_VALUE_ERROR)
-    # if value.isdigit():
-    #     convert_error_label.configure(fg='black')
-    # else:
-
-    # sleep(1000)
-    # convert_error_label.configure(fg='black')
+        convert_out_label.configure(fg='red')
+        convert_out_label.configure(text=config.NOT_NUMERIC_VALUE_ERROR)
 
 
 def get_data_from_cbr(date):
     answer = []
+
     response = urllib.request.urlopen("http://cbr.ru/scripts/XML_daily.asp")
     dom = xml.dom.minidom.parse(response)  # Получение DOM структуры айла
     dom.normalize()
@@ -74,7 +72,41 @@ def get_data_from_cbr(date):
     print(answer)
 
 
+def draw_currency_graph() -> None:
+    pass
+
+
+def date_to_dot_format(time) -> str:
+    return str(time.day) + '.' + str(time.month) + '.' + str(time.year)
+
+
+def change_selection_period() -> None:
+    now = datetime.datetime.now()
+    pr = radio_period_state.get()
+    a = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь',
+         'Декабрь']
+    data = []
+    delta = datetime.timedelta(days=1)
+    if pr == 1:
+        now -= 6 * delta
+        for i in range(3):
+            data.append(str(now.strftime('%d.%m.%Y') + '-' + (now + 6 * delta).strftime('%d.%m.%Y')))
+            now -= 7 * delta
+    elif pr == 2:
+        print(now.month)
+        for i in range(3):
+            data.append(a[now.month % 12 - 1] + ' ' + str(now.year))
+            now -= 30*delta
+
+    period_combo['values'] = data
+    period_combo.current(0)
+    # print(datetime)
+    # print(datetime.date.today())
+    # print(radio_period_state.get())
+
+
 if __name__ == '__main__':
+
     """
     Создание окна и вкладок
     """
@@ -91,9 +123,10 @@ if __name__ == '__main__':
     tab2 = ttk.Frame(tab_control)
 
     # Вкладки приложения
-    tab_control.add(tab1, text="Калькулятор валют")
+
     tab_control.add(tab2, text="Динамика курса")
-    tab_control.enable_traversal()
+    tab_control.add(tab1, text="Калькулятор валют")
+    # tab_control.enable_traversal()
 
     """
     Вкладка - Калькулятор валют
@@ -125,23 +158,54 @@ if __name__ == '__main__':
     currency_convert_btn.grid(column=3, row=1, padx=config.PADX, pady=config.PADY)
 
     # Лайбел вывода конвертации/ошибки
-    convert_error_label = Label(tab1, text="")  # Надпись
-    convert_error_label.grid(column=2, row=2, padx=config.PADX, pady=config.PADY)
+    convert_out_label = Label(tab1, text="")
+    convert_out_label.grid(column=2, row=2, padx=config.PADX, pady=config.PADY)
 
     """
     Вкладка - Динамика курса 
     """
 
     # Лайбел 'Валюта'
-    convert_error_label = Label(tab2, text="Валюта", compound=LEFT)
-    convert_error_label.grid(column=1, row=1, padx=config.PADX)
+    currency_static = Label(tab2, text="Валюта")
+    currency_static.grid(column=1, row=1, padx=config.PADX)
+
+    # Лайбел 'Период'
+    period_static = Label(tab2, text="Период")
+    period_static.grid(column=2, row=1, padx=config.PADX)
+
+    # Лайбел 'Выбор периода'
+    select_period_static = Label(tab2, text="Выбор периода")
+    select_period_static.grid(column=3, row=1, padx=config.PADX)
 
     # Селектор выбора валюты для построения графика
     currency_combo_graf = ttk.Combobox(tab2, state="readonly")
-    currency_combo_graf['values'] = get_currency_list()
-    currency_combo_graf.current(21 if config.SORT_CURRENCY_LIST else 0)  # В любом случае первая валюта - рубль
+    currency_combo_graf['values'] = get_currency_list(False)
+    currency_combo_graf.current(
+        random.randint(0, len(currency_combo_to['values']) - 1) if config.RANDOM_CURRENCY else 0)
     currency_combo_graf.grid(column=1, row=2, padx=config.PADX, pady=config.PADY)
 
+    # Селектор выбора валюты для построения графика
+    period_combo = ttk.Combobox(tab2, state="readonly")
+    period_combo.grid(column=3, row=2, padx=config.PADX, pady=config.PADY)
+
+    # Радио длительности периода
+    radio_period_state = IntVar()
+    radio_period_state.set(1)
+    change_selection_period()
+
+    radio_period_1 = Radiobutton(tab2, text='Неделя', value=1, variable=radio_period_state,
+                                 command=change_selection_period)
+    radio_period_2 = Radiobutton(tab2, text='Месяц', value=2, variable=radio_period_state,
+                                 command=change_selection_period)
+    radio_period_3 = Radiobutton(tab2, text='Квартал', value=3, variable=radio_period_state,
+                                 command=change_selection_period)
+    radio_period_4 = Radiobutton(tab2, text='Год', value=4, variable=radio_period_state,
+                                 command=change_selection_period)
+
+    radio_period_1.grid(column=2, row=2)
+    radio_period_2.grid(column=2, row=3)
+    radio_period_3.grid(column=2, row=4)
+    radio_period_4.grid(column=2, row=5)
 
     # matplotlib.use('TkAgg')
     # fig = plt.figure()
---
+++ tests.py
@@ -0,0 +1,12 @@
+import datetime
+
+if __name__ == '__main__':
+    now = datetime.datetime.now()
+
+    # print(datetime.datetime.now().strftime("%m/%d/%Y"))
+    print(datetime.datetime.now().strftime("%m/%d/%Y"))
+
+
+
+
+    pass
